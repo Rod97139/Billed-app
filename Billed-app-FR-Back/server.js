eae47f95e0546859f9474a50e94844fea3ee84e8
Rod97139 <fayaflame@gmail.com>
2024-02-19 06:26:00 +0100

    npm run test pdf

--- Billed-app-FR-Back/server.js
+++ Billed-app-FR-Back/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 app.listen(5678, () => {
   // eslint-disable-next-line no-console
-  console.log('Example app listening on port 5678!');
+  console.log('Billed app listening on port 5678!');
 });
